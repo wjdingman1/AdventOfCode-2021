3ef0724ff87d21e9bae411094a1263b46e4980f7
Wes Dingman <wdingman@asymmetrik.com>
2021-12-02 08:41:52 -0500

    Day two

---
+++ Day2/part1.py
@@ -0,0 +1,17 @@
+hor, ver, commands = 0, 0, []
+
+with open("input.txt", "r") as f:
+    commands = [line.rstrip() for line in f.readlines()]
+
+
+for x in commands:
+    direction, value = x.split(" ")
+    value = int(value)
+    if direction == "forward":
+        hor += value
+    elif direction == "down":
+        ver += value
+    else:
+        ver -= value
+
+print(ver * hor)
---
+++ Day2/part2.py
@@ -0,0 +1,23 @@
+hor, ver, aim, commands, = (
+    0,
+    0,
+    0,
+    [],
+)
+
+with open("input.txt", "r") as f:
+    commands = [line.rstrip() for line in f.readlines()]
+
+
+for x in commands:
+    direction, value = x.split(" ")
+    value = int(value)
+    if direction == "forward":
+        hor += value
+        ver += aim * value
+    elif direction == "down":
+        aim += value
+    else:
+        aim -= value
+
+print(ver * hor)
